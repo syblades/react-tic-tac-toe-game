c39fcc5961135595a4a7c46624f8dc10c6ac2b4d
syblades <86990570+syblades@users.noreply.github.com>
2021-12-16 18:42:53 -0800

    passed props from App.js

--- src/components/Square.js
+++ src/components/Square.js
@@ -4,13 +4,15 @@ import PropTypes from 'prop-types';
 import './Square.css';
 
 const Square = (props) => {
-  // For Wave 1 enable this 
-  //  Component to alert a parent 
-  //  component when it's clicked on.
-
-  return <button className='square'>{props.value}</button>;
+  const callback = () => {
+    props.onClickCallback(props.id);
+  };
+  return (
+    <button className="square" onClick={callback}>
+      {props.value}
+    </button>
+  );
 };
-
 Square.propTypes = {
   value: PropTypes.string.isRequired,
   onClickCallback: PropTypes.func.isRequired,
